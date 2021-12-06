6e7339e070e35086a2e93fe456c8cc7321bed1d3
provodoihi <profiloes@gmail.com>
2021-12-06 12:52:20 +0700

    Add mobx observer to location search screen

--- src/AppFeature/Common/LocationSearch/LocationScreen.tsx
+++ src/AppFeature/Common/LocationSearch/LocationScreen.tsx
@@ -15,112 +15,113 @@ import {useForm} from 'react-hook-form';
 import {useStores, LocationDataType} from '../../../models';
 import {styleLocationSearchScreen as style} from './style';
 import {pic_healthClinic, pic_notFound, pic_search} from '../../../../assets';
+import {observer} from 'mobx-react-lite';
 
-export const LocationScreen = ({
-  navigation,
-}: AppNavigationProps<'Location'>) => {
-  const [data, setData] = useState<Array<LocationDataType>>([]);
-  // const [status, setStatus] = useState<number>(0);
-  const [isVisible, setVisible] = useState<boolean>(false);
+export const LocationScreen = observer(
+  ({navigation}: AppNavigationProps<'Location'>) => {
+    const [data, setData] = useState<Array<LocationDataType>>([]);
+    // const [status, setStatus] = useState<number>(0);
+    const [isVisible, setVisible] = useState<boolean>(false);
 
-  const {locationStore} = useStores();
+    const {locationStore} = useStores();
 
-  interface SearchProps {
-    keyword: string;
-  }
-
-  const {control, handleSubmit} = useForm<SearchProps>({
-    defaultValues: {
-      keyword: '',
-    },
-    resolver: SearchSchema,
-  });
-
-  const onSubmit = async ({keyword}: SearchProps) => {
-    try {
-      setVisible(true);
-      let response = await locationStore.getLocationList(keyword);
-      setData(response);
-      setVisible(false);
-    } catch (error) {
-      setVisible(false);
-      showToast('Something went wrong');
+    interface SearchProps {
+      keyword: string;
     }
-  };
 
-  const renderItem = ({item}: ListRenderItemInfo<LocationDataType>) => {
-    return (
-      <ListItem
-        style={[style.buttonNoColor, style.shadowGray]}
-        activeOpacity={0.8}
-        onPress={() => {
-          navigation.navigate('MapView', {
-            name: item.name,
-            latitude: item.latitude,
-            longitude: item.longtitude,
-          });
-        }}
-        isMultipleAtrribute={true}
-        imageSource={pic_healthClinic}>
-        <Text style={style.textSmallBoldBlack}>{item.name}</Text>
-        <Text style={style.textSmallNormalBlack}>{item.address}</Text>
-        <Text style={style.textSmallNormalBlack}>{item.phone}</Text>
-      </ListItem>
-    );
-  };
+    const {control, handleSubmit} = useForm<SearchProps>({
+      defaultValues: {
+        keyword: '',
+      },
+      resolver: SearchSchema,
+    });
 
-  const Notfound = () => {
-    return (
-      <View style={style.midScreen}>
-        <Image style={style.image} source={pic_notFound} />
-        <Text style={[style.textAlignCenter, style.textBigBoldBlack]}>
-          Not found
-        </Text>
-      </View>
-    );
-  };
+    const onSubmit = async ({keyword}: SearchProps) => {
+      try {
+        setVisible(true);
+        let response = await locationStore.getLocationList(keyword);
+        setData(response);
+        setVisible(false);
+      } catch (error) {
+        setVisible(false);
+        showToast('Something went wrong');
+      }
+    };
 
-  const Init = () => {
-    return (
-      <View style={style.midScreen}>
-        <Image style={style.image} source={pic_search} />
-        <Text style={[style.textAlignCenter, style.textNormalBlack]}>
-          Search hospitals and clinics
-        </Text>
-      </View>
-    );
-  };
+    const renderItem = ({item}: ListRenderItemInfo<LocationDataType>) => {
+      return (
+        <ListItem
+          style={[style.buttonNoColor, style.shadowGray]}
+          activeOpacity={0.8}
+          onPress={() => {
+            navigation.navigate('MapView', {
+              name: item.name,
+              latitude: item.latitude,
+              longitude: item.longtitude,
+            });
+          }}
+          isMultipleAtrribute={true}
+          imageSource={pic_healthClinic}>
+          <Text style={style.textSmallBoldBlack}>{item.name}</Text>
+          <Text style={style.textSmallNormalBlack}>{item.address}</Text>
+          <Text style={style.textSmallNormalBlack}>{item.phone}</Text>
+        </ListItem>
+      );
+    };
 
-  return (
-    <View style={style.container}>
-      <HeaderBar text="Find Hospital Clinic" isBack={true} />
-      <View style={style.container2}>
-        <ModalLoad isVisibleLoad={isVisible} />
-        <View style={style.searchBox}>
-          <TextInputField
-            onSubmitEditing={handleSubmit(onSubmit)}
-            placeholder="Search here"
-            placeholderTextColor="#9FA5AA"
-            multiline={false}
-            controller={control}
-            isErrorField={false}
-            name="keyword"
-          />
+    const Notfound = () => {
+      return (
+        <View style={style.midScreen}>
+          <Image style={style.image} source={pic_notFound} />
+          <Text style={[style.textAlignCenter, style.textBigBoldBlack]}>
+            Not found
+          </Text>
         </View>
-        {locationStore.responseStatus === 200 ? (
-          <View style={style.midScreen}>
-            <FlatList
-              data={data}
-              renderItem={renderItem}
-              keyExtractor={item => `row-${item.id}`}
+      );
+    };
+
+    const Init = () => {
+      return (
+        <View style={style.midScreen}>
+          <Image style={style.image} source={pic_search} />
+          <Text style={[style.textAlignCenter, style.textNormalBlack]}>
+            Search hospitals and clinics
+          </Text>
+        </View>
+      );
+    };
+
+    return (
+      <View style={style.container}>
+        <HeaderBar text="Find Hospital Clinic" isBack={true} />
+        <View style={style.container2}>
+          <ModalLoad isVisibleLoad={isVisible} />
+          <View style={style.searchBox}>
+            <TextInputField
+              onSubmitEditing={handleSubmit(onSubmit)}
+              placeholder="Search here"
+              placeholderTextColor="#9FA5AA"
+              multiline={false}
+              controller={control}
+              isErrorField={false}
+              name="keyword"
             />
           </View>
-        ) : locationStore.responseStatus === 204 ? (
-          <Notfound />
-        ) : (
-          <Init />
-        )}
+          {locationStore.responseStatus === 200 ? (
+            <View style={style.midScreen}>
+              <FlatList
+                data={data}
+                renderItem={renderItem}
+                keyExtractor={item => `row-${item.id}`}
+              />
+            </View>
+          ) : locationStore.responseStatus === 204 ? (
+            <Notfound />
+          ) : (
+            <Init />
+          )}
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  },
+);
